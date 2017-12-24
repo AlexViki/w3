bfa8830aff8ce2f1c3cf30a70e96e2062d3c27e5
Alex Viki <shevashokc@gmail.com>
2017-12-24 13:26:21 +0200

    String Methods - Finding a String in a String (indexOf, lastIndexOf)
    * also were moved scripts to thml because if run from js-file the are some errors. Why are there errors - unknown at this moment

--- JavaScript String Methods/main.js
+++ JavaScript String Methods/main.js
@@ -2,5 +2,12 @@ var showLenghtFun = function () {
     var txt = "QWERTYUIOPKJHGFFDAA",
         sln = txt.length,
         tmp = document.getElementById('idP_1');
-    return tmp.innerHTML = "The lenght is: " + sln;
+    tmp.innerHTML = "The lenght is: " + sln;
+};
+
+var indexOfFun = function () {
+    var text1 = "\\My name is Alex\\";
+    var pos = text1.indexOf('is');
+    document.getElementById('idP_2').innerHTML = 'The string: ' + text1 + '<br> seach position of "is"';
+    document.getElementById('button2').innerHTML = 'The position of "is": '  + pos;
 };
