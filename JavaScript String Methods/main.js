5842e56ee9208e2a5d03e2b4aab760005277d37b
Alex Viki <shevashokc@gmail.com>
2017-12-24 12:09:05 +0200

    JavaScript String Methods - String Length

---
+++ JavaScript String Methods/main.js
@@ -0,0 +1,6 @@
+var showLenghtFun = function () {
+    var txt = "QWERTYUIOPKJHGFFDAA",
+        sln = txt.length,
+        tmp = document.getElementById('idP_1');
+    return tmp.innerHTML = "The lenght is: " + sln;
+};
