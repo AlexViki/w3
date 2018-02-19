742e14bf019e5001b1a87e79d1da6c8a51805d02
Alex Viki <shevashokc@gmail.com>
2018-02-19 21:04:10 +0200

    JavaScript Comparison, Logical Operators - Comparing Different Types;
    the case with converted to the proper type before comparison

--- JavaScript_Comparison_Logical_Operators/main.js
+++ JavaScript_Comparison_Logical_Operators/main.js
@@ -43,3 +43,45 @@ let doc2 = document.getElementById('id-4');
     };*/
     (age >= 1 && age <=120) ? ( age >= 18 ? doc2.innerHTML = 'You are Adult' : doc2.innerHTML = 'You are Young' ) : doc2.innerHTML = 'ERROR';
 };
+
+//Comparing Different Types
+let a1 = 9,
+    a2 = '18',
+    a3 = 'Alex',
+    a4 = 2,
+    a5 = '2';
+let doc5 = document.getElementById('id-5');
+
+doc5.innerHTML = 'We have three value: ' + '<br>Value1: ' + a1 + '; <b>Type: ' + (typeof a1) + '</b>' +
+    '<br>Value2: ' +a2 + '; <b>Type: ' + (typeof a2) + '</b>' +
+    '<br>Value3: ' + a3 + '; <b>Type: ' + (typeof a3) + '</b>' +
+    '<br>Value4: ' + a4 + '; <b>Type: ' + (typeof a4) + '</b>' +
+    '<br>Comparing numbers: ' + a1 + ' > ' + a4 + '; ' + (a1 > a4) +
+    '<br>Comparing number and string: ' + a4 + ' < ' + a2 + '; ' + (a4 < a2) + 
+    '<br>Comparing number and string: ' + a1 + ' < ' + a3 + '; ' + (a1 < a3) +
+    '<br>Comparing number and string: ' + a1 + ' > ' + a3 + '; ' + (a1 > a3) +
+    '<br>Comparing number and string: ' + a1 + ' == ' + a3 + '; ' + (a1 == a3) +
+    '<br>Comparing string and string: ' + a5 + ' > ' + a2 + '; ' + (a5 > a2) +
+    '<br>Comparing string and string: ' + a5 + ' < ' + a2 + '; ' + (a5 < a2) +
+    '<br>Comparing string and string: ' + a5 + ' == ' + a2 + '; ' + (a5 == a2) + 
+    '<br>When comparing two strings, "' + a5 + '" will be greater than "' + a2 + '", because (alphabetically) 1 is less than 2.';
+
+//button checkAge2()
+function checkAge2() {
+    let doc6 = document.getElementById('id-6'),
+        age2 = document.getElementById('inputAge2').value;
+    //age2 is not a number (if string "isNaN" return true)
+    (isNaN(age2)) ? ( doc6.innerHTML = 'Value is not a number!!!') :
+        //you can not have age less than 0
+        (age2 < 0 ? doc6.innerHTML = 'ERROR: age < 0 !!! ' : 
+            //you can not have age bigger then 120
+            (age2 >= 120 ? doc6.innerHTML = 'ERROR: age > 120 !!! ' : 
+                //checking who are you, young or adult
+                ( age2 >= 18 ? doc6.innerHTML = 'You are adult!' : 
+                 doc6.innerHTML = 'You are young :)' 
+                )
+            )
+        );  
+};
+  
+    
