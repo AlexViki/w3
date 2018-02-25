ac73d4aa64f11a9c52fe3eb1f2acf6844b585851
Alex Viki <shevashokc@gmail.com>
2018-02-25 16:41:38 +0200

    JavaScript For Loop - The For Loop

--- JavaScript_For_Loop/main.js
+++ JavaScript_For_Loop/main.js
@@ -4,7 +4,34 @@ function runLoop_1() {
     let colors = ['red', 'green', 'blue'];
     for (let i = 0; i < colors.length; i++) {
         //console.log(doc);
-        document.getElementById("id_" + (i + 1)).innerHTML = text1 + i + '; Value is : ' + colors[i] + ';<br>';
-        document.getElementById('id_'+ (i + 1)).style.color = colors[i];    
+        document.getElementById('idRunLoop_' + (i + 1)).innerHTML = text1 + i + '; Value is : ' + colors[i] + ';<br>';
+        document.getElementById('idRunLoop_'+ (i + 1)).style.color = colors[i];    
     };
+};
+
+//The For Loop
+function runLoopFor_1() {
+    let inputValue_1 = document.getElementById('inputId_1').value;
+    let doc_2 = document.getElementById('id_2');
+    doc_2.innerHTML = '';
+    if (inputValue_1 <= 100 && inputValue_1 >= -100) {
+        for (let i = 0; i <= inputValue_1; i++) {
+            doc_2.innerHTML += i + '; ';
+        };
+    } else { 
+        doc_2.innerHTML += 'ERROR! Type more -100 or less 100';
+        };
+};
+function runLoopFor_2() {
+    let inputValue_2 = document.getElementById('inputId_2').value;
+    let doc_3 = document.getElementById('id_3');
+    let i = 0;
+    doc_3.innerHTML = '';
+    if (inputValue_2 <= 100 && inputValue_2 >= -100) {
+        for (; i <= inputValue_2; i++) {
+            doc_3.innerHTML += i + '; ';
+        };
+    } else { 
+        doc_3.innerHTML += 'ERROR! Type more -100 or less 100';
+        };
 };
