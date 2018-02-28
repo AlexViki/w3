7bcd0c5ad5dd41c7627030b03f75b251beb3ce21
Alex Viki <shevashokc@gmail.com>
2018-02-28 19:11:08 +0200

    JavaScript Break and Continue - The Continue Statement

--- JavaScript_Break_and_Continue/main.js
+++ JavaScript_Break_and_Continue/main.js
@@ -1,12 +1,25 @@
 // The Break Statement
 function funWithBreakre() {
     let doc_1 = document.getElementById('id_1');
-    
     for(let i = 0; i < 10; i++) {
         if(i === 5) { 
             break;
         };
-        doc_1.innerHTML = 'The breack was executed when i = ' + i ; 
+        doc_1.innerHTML = 'The breack was executed when i = ' + i; 
     };
 };
 funWithBreakre();
+
+//The Continue Statement
+function funWithContinue() {
+    let doc_2 = document.getElementById('id_2');
+    for(let j = 0; j < 10; j++){
+        if ( j === 5 ) {
+            doc_2.innerHTML += '<b>Skiped: ' + j + '</b><br>';
+            continue;
+        };
+    doc_2.innerHTML += 'Iteration: ' + j + '<br>';
+    console.log(j);
+    };
+};
+funWithContinue();
