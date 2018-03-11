15a674bbc6a2532c8bfb4bfe6e44b02d56bfbdca
Unknown <shevashokc@gmail.com>
2018-03-11 10:58:29 +0200

    JavaScript Errors - Input Validation (try, thro, catch)
    Input Validation (try, thro, catch)

--- JavaScript Errors_Throw_Try_Catch/main.js
+++ JavaScript Errors_Throw_Try_Catch/main.js
@@ -1,7 +1,7 @@
 //Errors Will Happen
 //runWillHappen();
 document.getElementById('button_1').onclick = function() {
-    try { 
+    try {
         addlert("Hi! I'm Alex!!!");
         //c = a + b;
     }
@@ -18,9 +18,29 @@ document.getElementById('button_r1').onclick = function() {
     finally { alert('Runing before "try" and "catch"!!!') }
 };
 document.getElementById('button_r2').onclick = function() {
-    try { 
+    try {
         document.getElementById('id_2').innerHTML = '<br>There is NOT any ERROR in <b>try</b>';
     }
     catch(e) { document.getElementById('id_2').innerHTML = '<br>There is ERROR in <b>try</b>: ' + e.message; }
     finally { alert('Runing before "try" and "catch"!!!') }
+};
+
+//Input Validation Example
+runValidExm();
+function runValidExm() {
+    let inp5 = document.getElementById('input_5').value;
+    let doc5 = document.getElementById('id_5');
+    //console.log(inp5);
+    doc5.innerHTML = '';
+    try {
+        if (inp5 == '') { throw 'Empty'; }
+        if (isNaN(inp5)) { throw 'not a number!!!'; }
+        inp5 = Number(inp5);
+        if (inp5 < 9) { throw 'less then 10'; }
+        if (inp5 > 19) { throw 'more the 20'; }
+    }
+    catch (e) {
+        doc5.innerHTML = 'The ERRO is: ' + e + '; Inputed: ' + inp5;
+        alert(e);
+    };
 };
