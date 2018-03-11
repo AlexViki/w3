7783c09e0620c25b504fa50ed34b8eb9d1cde354
Unknown <shevashokc@gmail.com>
2018-03-11 15:57:37 +0200

    JavaScript Errors - The Error Object, Error Name Values

--- JavaScript Errors_Throw_Try_Catch/main.js
+++ JavaScript Errors_Throw_Try_Catch/main.js
@@ -36,8 +36,8 @@ function runValidExm() {
         if (inp5 == '') { throw 'Empty'; }
         if (isNaN(inp5)) { throw 'not a number!!!'; }
         inp5 = Number(inp5);
-        if (inp5 < 9) { throw 'less then 10'; }
-        if (inp5 > 19) { throw 'more the 20'; }
+        if (inp5 <= 10) { throw 'less then 10'; }
+        if (inp5 >= 20) { throw 'more the 20'; }
     }
     catch (e) {
         doc5.innerHTML = 'The ERRO is: ' + e + '; Inputed: ' + inp5;
@@ -62,12 +62,59 @@ function runFinallyStat() {
         if (in7 == '') throw 'ERROR: Empty value';
         if (isNaN(in7)) throw 'ERROR: not a number!!!'
         in7 = Number(in7);
-        if (in7 < 10) throw 'ERROR: less than 10';
-        if (in7 > 20) throw 'ERROR: more than 20';
+        if (in7 <= 10) throw 'ERROR: less than 10';
+        if (in7 >= 20) throw 'ERROR: more than 20';
     } catch (e) {
         doc7.innerHTML = 'The result: ' + e;
     } finally {
         //console.log(in7);
         doc7.innerHTML += '<br>Block of code to be executed regardless of the try / catch result. Value: ' + in7;
+    }
+};
+
+//Error Name Values
+//Range Error
+function runRanfeError() {
+    let numA = 67;
+    document.getElementById('id_9_2').innerHTML = '67.toPrecision(500)' + '<br>';
+    try {
+        numA.toPrecision(500);
+    } catch (e) {
+        document.getElementById('id_9_2').innerHTML += 'The result is: ' + e.name;
+    }
+};
+//Reference Error
+function runReferenceError() {
+    let numB;
+    try {
+        numB = q + 1;
+    } catch (e) {
+        document.getElementById('id_9_3').innerHTML ='The result is: ' + e.name;
+    }
+};
+//Syntax Error
+function runSyntaxError() {
+    try {
+        eval("alert('Hello!!!)");
+    } catch (e) {
+        document.getElementById('id_9_4').innerHTML = 'The result is: ' + e.name;
+    }
+};
+//Type Error
+function runTypeError() {
+    let numC = 12;
+    try {
+        numC.toUpperCase();
+    } catch (e) {
+        document.getElementById('id_9_5').innerHTML = 'The result is: ' + e.name + '<br>' + e.message;
+    }
+};
+//URI Error
+function runURIError() {
+    try {
+        decodeURI('%%%');
+    } catch (e) {
+        document.getElementById('id_9_6').innerHTML = 'The result is: ' + e.name + '<br>' + e.message +
+        '<br>' + e.stack;
     }
 };
