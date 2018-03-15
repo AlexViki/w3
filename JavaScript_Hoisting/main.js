5476d2ffb467eeccd3c1cf537c12b9946c5c4672
Alex Viki <shevashokc@gmail.com>
2018-03-15 09:30:55 +0200

    JavaScript Use Strict - Declaring Strict Mode

--- JavaScript_Hoisting/main.js
+++ JavaScript_Hoisting/main.js
@@ -9,3 +9,22 @@ function runHosting_1() {
     document.getElementById('id_1').innerHTML += '<br>The varible was declared and then assigned: <b>' + y + '</b>' +
     '<br>To understand this, we have to understand the term <b>"hoisting"</b>.';
 }
+
+//JavaScript Initializations are Not Hoisted
+function runIniNotHosting() {
+    var num1 = 10,
+        num2 = 20;
+document.getElementById('id_2').innerHTML = 'The varibles were declareted. Num1 = ' + num1 + '; Num2: ' + num2 + '; Num3: ' + num3;
+var num3 = 30;
+    
+var numA = 'A';
+var numB;
+document.getElementById('id_2').innerHTML += '<br>Example2: ' + numA + '; ' + numB;
+numB = 'B';
+}
+
+//run2();
+var run2 = function() {
+    console.log('Runing');
+}
+run2();
