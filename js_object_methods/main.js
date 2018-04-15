1ca96cc1670d3c4b92c4264fd436f815daa8e9a5
Alex777 <shevashokc@gmail.com>
2018-04-15 12:41:14 +0300

    JS Object Methods - Adding a Method to an Object

--- js_object_methods/main.js
+++ js_object_methods/main.js
@@ -15,6 +15,31 @@ function accessObjMethod() {
 	doc2.innerHTML = user.fullName();
 	doc2.innerHTML += '<br>' + 'If you access the fullName property, without (), it will return the function definition: ' + '<br>' +
 		user.fullName;
+}
+
+//Adding a Method to an Object
+window.onload = addMethod();
+function addMethod() {
+	var doc4 = document.getElementById('id_4');
+	var btnAddMeth = document.getElementById('btnAddMeth');
+	var inp1 = document.getElementById('inp1');
+	var user = {
+		fName: "Alex",
+		sName: "Viki",
+		age: 30
+		};
+	var tmp;
+	doc4.setAttribute('class','style-result');
+	btnAddMeth.onclick = function () {
+		user.inp1 = function() {
+			return "I'm " + this.fName + "; I'v a new method.";
+		};
+		console.log(user.ee);
+		for (tmp in user) {
+			doc4.innerHTML += user[tmp] + "<br>";
+		}
+		doc4.innerHTML += user.inp1();
+	}
 
 
 }
